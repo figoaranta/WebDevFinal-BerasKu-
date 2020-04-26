0afa8a9149838c65605bc34008cbb830a20c6865
Figo <figoaranta@gmail.com>
2020-04-26 14:55:17 +0700

    posts table added

---
+++ database/migrations/2020_04_26_072308_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->integer('userId')->unsigned();
+            $table->foreign('userId')->references('id')->on('accounts')->onDelete('cascade');
+            $table->integer('productId')->unsigned();
+            $table->foreign('productId')->references('id')->on('products')->onDelete('cascade');
+            $table->string('postTitle');
+            $table->string('postDescription');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
