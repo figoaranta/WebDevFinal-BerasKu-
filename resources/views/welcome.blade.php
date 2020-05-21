a3818d4c0f7d113b01c39314493873953d223d90
Figo <figoaranta@gmail.com>
2020-05-21 19:41:25 +0700

    testApp

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,183 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-    <title>Login Page</title>
+    <!-- Required meta tags-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Colorlib Templates">
+    <meta name="author" content="Colorlib">
+    <meta name="keywords" content="Colorlib Templates">
+
+    <!-- Title Page-->
+    <title>BERASKU Registration Form</title>
+
+    <!-- Icons font CSS-->
+    <link href="vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+    <link href="vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
+    <!-- Font special for pages-->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Vendor CSS-->
+    <link href="vendor/select2/select2.min.css" rel="stylesheet" media="all">
+    <link href="vendor/datepicker/daterangepicker.css" rel="stylesheet" media="all">
+
+    <!-- Main CSS-->
+    <link href="css/main.css" rel="stylesheet" media="all">
+
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"
+integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 </head>
+
+<script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
 <body>
-    <h1>Welcome To BerasKu</h1>
-    <h2>Please login to see greatness</h2>
-    <form action="https://berasku.herokuapp.com/api/auth/login" method="POST">
-         <!-- {{ csrf_field() }} -->
-    <label >email :</label>
-    <input type="text" name="email">
-    <label >password :</label>
-    <input type="text" name="password">
-    <button class="btn btn--radius-2 btn--blue" type="submit" value="submit">Submit</button>
-    </form>
-</body>
+    <div class="page-wrapper bg-gra-02 p-t-130 p-b-100 font-poppins">
+        <div class="wrapper wrapper--w680">
+            <div class="card card-4">
+                <div class="card-body">
+                    <h2 class="title">BERASKU Registration Form</h2>
+                    <form id="regis-form">
+                        <div class="row row-space">
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <label class="label">First name</label>
+                                    <input class="input--style-4" type="text" id="first_name">
+                                </div>
+                            </div>
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <label class="label">Last name</label>
+                                    <input class="input--style-4" type="text" id="last_name">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row row-space">
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <label class="label">NIK</label>
+                                    <input class="input--style-4" type="text" id="NIK">
+                                </div>
+                            </div>
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <label class="label">dateofobirth</label>
+                                    <input class="input--style-4" type="text" id="dateofbirth">
+                                </div>
+                                <!-- <div class="input-group">
+                                    <label class="label">Date of Birth</label>
+                                    <div class="input-group-icon">
+                                        <input class="input--style-4 js-datepicker" type="text" id="dateofbirth">
+                                        <i class="zmdi zmdi-calendar-note input-icon js-btn-calendar"></i>
+                                    </div>
+                                </div> -->
+                            </div>
+                            <div class="row row-space">
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <label class="label">Username</label>
+                                    <input class="input--style-4" type="text" id="username">
+                                </div>
+                            </div>
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <label class="label">Password</label>
+                                    <input class="input--style-4" type="text" id="password">
+                                </div>
+                            </div>
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <label class="label">Address</label>
+                                    <input class="input--style-4" type="text" id="address">
+                                </div>
+                            </div>
+                        </div>
+                        </div>
+                        <div class="row row-space">
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <label class="label">Email</label>
+                                    <input class="input--style-4" type="text" id="email">
+                                </div>
+                            </div>
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <label class="label">Phone Number</label>
+                                    <input class="input--style-4" type="text" id="phone">
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class="row row-space">
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <label class="label">Customer/Shop</label>
+                                    <input class="input--style-4" type="text" id="customer">
+                                </div>
+                            </div>
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <label class="label">Gender Type</label>
+                                    <input class="input--style-4" type="text" id="genderType">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="p-t-15">
+                            <button class="btn btn--radius-2 btn--blue" type="submit" value="submit">Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        
+$('#regis-form').on('submit',function(){
+        var namapertama=$("#first_name").val();
+        var lastname = $("#last_name").val();
+        var username = $("#username").val();
+        var email = $("#email").val();
+        var NIK = $("#NIK").val();
+        var dateofbirth = $("#dateofbirth").val();
+        var address = $("#address").val();
+        var phonenumber = $("#phone").val();
+        var genderType = $("#genderType").val();
+        var password = $("#password").val();
+        var usertype = $("#customer").val();
+        var dataJson = JSON.stringify({
+            "firstName":namapertama,
+            "lastName":lastname,
+            "userName":username,
+            "email" : email,
+            "NIM":NIK,
+            "dateOfBirth":dateofbirth,
+            "address":address,
+            "phoneNumber":phonenumber,
+            "userGenderType" :genderType,
+            "userType":usertype,
+            "password":password
+        });
+        dataJson = JSON.parse(dataJson);
+        document.write(dataJson);
+        $.post("https://berasku.herokuapp.com/api/v1/account",dataJson);
+        
+    });
+        
+    </script>
+
+
+    <!-- Jquery JS-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <!-- Vendor JS-->
+    <script src="vendor/select2/select2.min.js"></script>
+    <script src="vendor/datepicker/moment.min.js"></script>
+    <script src="vendor/datepicker/daterangepicker.js"></script>
+
+    <!-- Main JS-->
+    <script src="js/global.js"></script>
+
+</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+
 </html>
+<!-- end document-->
