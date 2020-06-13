dc1c621adaaa612aa1dbd32dcbb1c926d20a89f6
Figo <figoaranta@gmail.com>
2020-06-13 22:03:31 +0700

    put welcome page

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('testFileUpload');
+    return view('welcome');
 });
 
 Route::prefix('/accountImageCRUD')->group(function(){
