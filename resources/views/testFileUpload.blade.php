2132e0d97b54feccc3edeb90068848e2d6557263
Figo <figoaranta@gmail.com>
2020-06-13 22:04:14 +0700

    put welcome page

--- resources/views/testFileUpload.blade.php
+++
@@ -1,6 +0,0 @@
-<form action="{{route('upload')}}" method="POST" enctype="multipart/form-data">
-	@csrf
-	<input type="text" name="id">
-	<input type="file" name="profilePic">
-	<input type="submit">
-</form>
